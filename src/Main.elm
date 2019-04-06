0e22810cd58612c7743ca6c6d756f5db7ddd05b4
Hendrik Lammers <lammers@gmail.com>
2019-04-06 14:46:56 +0700

    Add Subscriptions module

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Events exposing (onKeyUp)
 import Html.Styled exposing (toUnstyled)
 import Http
-import Json.Decode as Decode exposing (Decoder)
 import Palette exposing (paletteListDecoder)
-import Ports exposing (confirmCopy)
 import Settings exposing (settings)
+import Subscriptions exposing (subscriptions)
 import Update exposing (Model(..), Msg(..), Navigation(..), update)
 import View exposing (view)
 
@@ -15,50 +13,11 @@ import View exposing (view)
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( Init
-    , get
-    )
-
-
-get : Cmd Msg
-get =
-    Http.get
+    , Http.get
         { url = settings.api
         , expect = Http.expectJson ReceiveData paletteListDecoder
         }
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.batch
-        [ onKeyUp keyDecoder
-        , confirmCopy CopyConfirmation
-        ]
-
-
-keyDecoder : Decode.Decoder Msg
-keyDecoder =
-    Decode.map
-        (\key ->
-            case key of
-                "ArrowRight" ->
-                    Navigate Next
-
-                "ArrowLeft" ->
-                    Navigate Previous
-
-                "ArrowUp" ->
-                    Navigate Previous
-
-                "ArrowDown" ->
-                    Navigate Next
-
-                "Enter" ->
-                    Navigate Random
-
-                _ ->
-                    NoOp
-        )
-        (Decode.field "key" Decode.string)
+    )
 
 
 main : Program () Model Msg
---
+++ src/Subscriptions.elm
@@ -0,0 +1,40 @@
+module Subscriptions exposing (subscriptions)
+
+import Browser.Events exposing (onKeyUp)
+import Json.Decode as Decode exposing (Decoder)
+import Ports exposing (confirmCopy)
+import Update exposing (Model, Msg(..), Navigation(..))
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ onKeyUp keyDecoder
+        , confirmCopy CopyConfirmation
+        ]
+
+
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+    Decode.map
+        (\key ->
+            case key of
+                "ArrowRight" ->
+                    Navigate Next
+
+                "ArrowLeft" ->
+                    Navigate Previous
+
+                "ArrowUp" ->
+                    Navigate Previous
+
+                "ArrowDown" ->
+                    Navigate Next
+
+                "Enter" ->
+                    Navigate Random
+
+                _ ->
+                    NoOp
+        )
+        (Decode.field "key" Decode.string)
