de5bbf567285c3ac16f337df6bca95baa496a7e1
Hendrik Lammers <lammers@gmail.com>
2019-05-20 21:05:11 +0700

    Test paletteListDecoder

--- tests/PaletteTest.elm
+++ tests/PaletteTest.elm
@@ -2,7 +2,7 @@ module PaletteTest exposing (suite)
 
 import Expect
 import Json.Decode as Decode
-import Palette exposing (Palette, paletteDecoder)
+import Palette exposing (Palette, paletteDecoder, paletteListDecoder)
 import Test exposing (..)
 
 
@@ -10,7 +10,103 @@ suite : Test
 suite =
     describe "Palette module"
         [ decodesJsonPalette
+        , decodesJsonList
         ]
+
+
+decodesJsonList : Test
+decodesJsonList =
+    test "Decodes json string with list of palettes" <|
+        \_ ->
+            let
+                json =
+                    """
+[{
+    "colors":[
+      "69D2E7",
+      "A7DBD8",
+      "E0E4CC",
+      "F38630",
+      "FA6900"
+    ],
+    "colorWidths":[
+      0.2,
+      0.2,
+      0.2,
+      0.2,
+      0.2
+    ],
+    "url":"http://www.colourlovers.com/palette/92095/Giant_Goldfish"
+  },
+  {
+    "colors":[
+      "FE4365",
+      "FC9D9A",
+      "F9CDAD",
+      "C8C8A9",
+      "83AF9B"
+    ],
+    "colorWidths":[
+      0.23,
+      0.07,
+      0.06,
+      0.07,
+      0.57
+    ],
+    "url":"http://www.colourlovers.com/palette/629637/()"
+  },
+  {
+    "colors":[
+      "ECD078",
+      "D95B43",
+      "C02942",
+      "542437",
+      "53777A"
+    ],
+    "colorWidths":[
+      0.2,
+      0.2,
+      0.2,
+      0.2,
+      0.2
+    ],
+    "url":"http://www.colourlovers.com/palette/694737/Thought_Provoking"
+  }]
+"""
+
+                palettes =
+                    [ { colors =
+                            [ "69D2E7"
+                            , "A7DBD8"
+                            , "E0E4CC"
+                            , "F38630"
+                            , "FA6900"
+                            ]
+                      , widths = [ 0.2, 0.2, 0.2, 0.2, 0.2 ]
+                      }
+                    , { colors =
+                            [ "FE4365"
+                            , "FC9D9A"
+                            , "F9CDAD"
+                            , "C8C8A9"
+                            , "83AF9B"
+                            ]
+                      , widths = [ 0.23, 0.07, 0.06, 0.07, 0.57 ]
+                      }
+                    , { colors =
+                            [ "ECD078"
+                            , "D95B43"
+                            , "C02942"
+                            , "542437"
+                            , "53777A"
+                            ]
+                      , widths = [ 0.2, 0.2, 0.2, 0.2, 0.2 ]
+                      }
+                    ]
+            in
+            Expect.equal
+                (Decode.decodeString paletteListDecoder json)
+                (Ok palettes)
 
 
 decodesJsonPalette : Test
